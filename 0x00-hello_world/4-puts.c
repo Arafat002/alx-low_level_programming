8dd85cd182c83bb0ac5894cae68518203e839838
Arafat Motolani <mhyzdamilola@gmail.com>
2022-10-13 14:19:34 -0700

    betty why me

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  *  * main - prints the phrase "Programming is like building a multilingual puzzle
  *   * Return: 0 if exited properly, non-zero otherwise
  *    */
-int main(void)
+int main()
 {
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
