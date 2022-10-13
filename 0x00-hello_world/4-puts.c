9f8d9dfbd1468c64d1c8160c6dd3a90876fcccba
Arafat Motolani <mhyzdamilola@gmail.com>
2022-10-13 14:14:22 -0700

    rectified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,24 +1,11 @@
 #!/bin/bash
 #include <stdio.h>
-
-
-
-/**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-int main(void)
-
+int main()
+/*
+ * main:serves as the entry point for the program
+ */
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 
 }
