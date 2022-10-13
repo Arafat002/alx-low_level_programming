2a0f13da4f3749e66c8fd8cba0c4dae5dc7fb514
Arafat Motolani <mhyzdamilola@gmail.com>
2022-10-13 14:04:21 -0700

    Puts c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Return: Always 0
+  */
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}	
