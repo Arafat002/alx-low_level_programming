146e47999a7a123bfffcab46770b9aa2e910843d
Arafat Motolani <mhyzdamilola@gmail.com>
2022-10-13 14:18:27 -0700

    betty why me

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #!/bin/bash
 #include <stdio.h>
-int main()
-/*
- * main:serves as the entry point for the program
- */
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+#include <stdio.h>
 
+/**
+ *  * main - prints the phrase "Programming is like building a multilingual puzzle
+ *   * Return: 0 if exited properly, non-zero otherwise
+ *    */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
