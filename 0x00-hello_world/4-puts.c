ff28097589f7619da0f75a6580510df76d0c239f
Arafat Motolani <mhyzdamilola@gmail.com>
2022-10-13 14:09:02 -0700

    rectified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,24 @@
 #!/bin/bash
 #include <stdio.h>
 
+
+
 /**
-  * main - Entry point
-  * Return: Always 0
-  */
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
-}	
+			return (0);
+
+}
