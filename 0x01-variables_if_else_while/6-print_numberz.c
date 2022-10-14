6d6502babd6537371e952311709ebabf94945d8c
Arafat Motolani <mhyzdamilola@gmail.com>
2022-10-14 14:44:13 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
