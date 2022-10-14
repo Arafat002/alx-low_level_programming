ea9411470c02be15f010330f3c28f3e7611e3602
Arafat Motolani <mhyzdamilola@gmail.com>
2022-10-14 15:23:48 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints all the possible comb of single-digit numbers, seperated by ,
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
