72aa5ec3a773867ace8e2edf7dba341668407a5d
Arafat Motolani <mhyzdamilola@gmail.com>
2022-10-14 14:53:32 -0700

    task-7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -prints the lowercase alphabet in reverse.
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
