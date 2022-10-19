544ce70915754fff191b20f206d9ad0e5cfa37c2
Arafat Motolani <mhyzdamilola@gmail.com>
2022-10-19 04:58:57 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -41,3 +41,9 @@ void print_times_table(int n)
 					_putchar(product / 100 + '0');
 					_putchar((product / 10) % 10 + '0');
 					_putchar(product % 10 + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
