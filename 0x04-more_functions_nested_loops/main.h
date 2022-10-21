0caa5b84930b8bc28723f2f54d8e7eff0456e292
Arafat Motolani <mhyzdamilola@gmail.com>
2022-10-21 07:02:54 -0700

    task 4

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,11 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+int _putchar(char c);
 int _isupper(int);
 int _isdigit(int);
 int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void);
 
 #endif
