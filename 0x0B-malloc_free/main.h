61f9533c49e170bde5b8307c35755a1e43f89d97
Arafat Motolani <mhyzdamilola@gmail.com>
2022-11-07 05:43:49 -0800

    task 0

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0B-malloc_free directory.
+ */
+
+char *create_array(unsigned int size, char c);
+
+#endif
